8488de47100549a33e2a1434eb6d91362cdd91f3
Chris Brocklesby <chrisbrocklesby@users.noreply.github.com>
2020-04-18 14:08:06 +1200

    Update

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const http = require('http');
 
-exports.apifunctions = function() {
-    const http = require('http');
-
-function http(req, res) {
-    http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        res.end('Send this')
-    }
+// export const server = ((req, res) => {
+//     http.createServer((req, res) => {
+//     if (req.method === 'GET') {
+//         res.end('Send this')
+//     }
     
-    if (req.method === 'POST') {
-    }
-});
-}
-server.listen(3000);
-}
+//     if (req.method === 'POST') {
+//     }
+// }
