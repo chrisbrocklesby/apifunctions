281c9646a522b8be5f7405b6ab2ce08a3fb3687a
Chris Brocklesby <chrisbrocklesby@users.noreply.github.com>
2020-04-18 13:39:05 +1200

    start

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const http = require('http');
+
 exports.apifunctions = function() {
-    console.log("API Function");
+    const http = require('http');
+
+function http(req, res) {
+    http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        res.end('Send this')
+    }
+    
+    if (req.method === 'POST') {
+    }
+});
+}
+server.listen(3000);
 }
